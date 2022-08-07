dfdc9124a84a6e4eafe89eb815ee3b0ae8deb28f
Hung Tran <Hung_Tran@epam.com>
2022-08-07 17:39:39 +0700

    refactor

--- app/screens/Login/Login.tsx
+++ app/screens/Login/Login.tsx
@@ -1,38 +1,41 @@
 import { useEffect } from 'react';
-import { View, Linking } from 'react-native';
+import { View, Linking, Alert } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import { styles } from './LoginStyles';
 
+// TODO: MOVE TO ENV
+const AUTH_URL = 'http://localhost:3000/auth/facebook';
+const URL_TYPE = 'url';
+
 export const LoginScreen = () => {
   useEffect(() => {
-    Linking.addEventListener('url', (url) => handleOpenURL(url.url));
-    Linking.getInitialURL().then((url: any) => {
-      if (url) {
-        handleOpenURL({ url });
-      }
-    });
+    Linking.addEventListener(URL_TYPE, (payload: { url: string }) => handleOpenURL(payload.url));
+
     return () => {
-      Linking.removeAllListeners('url');
+      Linking.removeAllListeners(URL_TYPE);
     };
   }, []);
 
-  const handleOpenURL = (url: any) => {
-    const endcodedURL = encodeURI(url);
-
-    const user = decodeURI(endcodedURL);
-
+  const handleOpenURL = (url: string) => {
+    //TODO: DECODE URL BY "&" : NEED RESEARCH
+    const user = decodeURI(url).match(/firstName=([^#]+)\/lastName=([^#]+)\/email=([^#]+)/);
+    //TODO: SAVE TO REDUX
     console.log('user', user);
   };
 
-  const openUrl = (url: string) => {
-    Linking.openURL(url);
+  const openUrl = () => async () => {
+    const isSupportedURL = await Linking.canOpenURL(AUTH_URL);
+    if (isSupportedURL) {
+      return Linking.openURL(AUTH_URL);
+    }
+    Alert.alert('Can not open this url');
   };
 
   return (
     <>
       <View style={styles.button}>
-        <Button mode="contained" onPress={() => openUrl('http://localhost:3000/auth/facebook')}>
+        <Button mode="contained" onPress={openUrl}>
           Login With Facebook
         </Button>
       </View>
