66d549e2b1f32d58da588c047911f0f127f272c0
Hung Tran <Hung_Tran@epam.com>
2022-09-13 11:32:15 +0700

    add config

--- app/configs/index.ts
+++ app/configs/index.ts
@@ -1,1 +1,2 @@
 export { default as linking } from './linking';
+export * from './urls';
--- app/providers/WebSocketProvider.tsx
+++ app/providers/WebSocketProvider.tsx
@@ -1,12 +1,14 @@
 import { createContext, PropsWithChildren } from 'react';
 import socketIO, { Socket } from 'socket.io-client';
 
+import { BASE_URL_SOCKET } from '@Configs/index';
+
 const WebSocketContext = createContext<Socket>(null as any);
 
 export { WebSocketContext };
 
 export default ({ children }: PropsWithChildren<{}>) => {
-  const socket = socketIO('http://localhost:3001');
+  const socket = socketIO(BASE_URL_SOCKET);
   socket.connect();
 
   return <WebSocketContext.Provider value={socket}>{children}</WebSocketContext.Provider>;
