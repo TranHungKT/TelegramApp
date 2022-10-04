963cc0a955c80ca6faff4b97bc1bb15d9e0f392d
Hung Tran <tranhung26122612@gmail.com>
2022-10-04 09:04:02 +0700

    refactor connect socket

--- app/configs/urls.ts
+++ app/configs/urls.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'http://localhost:3000/';
-export const BASE_URL_SOCKET = 'http://localhost:3001';
+export const BASE_URL_SOCKET = 'http://localhost:3000/socket';
--- app/providers/WebSocketProvider.tsx
+++ app/providers/WebSocketProvider.tsx
@@ -1,8 +1,11 @@
 import { createContext } from 'react';
 import socketIO, { Socket } from 'socket.io-client';
 
-import { BASE_URL_SOCKET } from '@Configs/index';
+import { BASE_URL } from '@Configs/index';
 
-export const socket = socketIO(BASE_URL_SOCKET).connect();
+export const socket = socketIO(BASE_URL, {
+  path: '/socket',
+  transports: ['websocket'],
+}).connect();
 
 export const WebSocketContext = createContext<Socket>(null as any);
