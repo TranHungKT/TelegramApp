3a59f4b2698966387e36ee2c06e4d6eea2a606c8
Hung Tran <tranhung26122612@gmail.com>
2022-10-25 16:20:03 +0700

    update listen to event

--- app/hooks/useSocket.ts
+++ app/hooks/useSocket.ts
@@ -1,8 +1,10 @@
 import { useContext, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import { SOCKET_EVENTS } from '@Constants/index';
 import { NewMessageFromSocket, ReadMessagePayload, TypingEventPayload } from '@Models/index';
 import { WebSocketContext } from '@Providers/index';
+import { userIdSelector } from '@Stores/user';
 
 import { useReduxForReadMessageStatus } from './useReduxForReadMessageStatus';
 import { useReduxForTypingEvent } from './useReduxForTypingEvent';
@@ -11,19 +13,26 @@ import { useReduxToUpdateMessageStatus } from './useReduxToUpdateMessageStatus';
 
 export const useSocket = () => {
   const socket = useContext(WebSocketContext);
+
+  const userId = useSelector(userIdSelector);
+
   const handleAddNewMessage = useReduxToAddNewMessage();
   const [handleTypingEvent, handleUnTypingEvent] = useReduxForTypingEvent();
   const [handleMessageReceived] = useReduxToUpdateMessageStatus();
   const [handleMessageRead] = useReduxForReadMessageStatus();
 
   useEffect(() => {
-    socket.on(SOCKET_EVENTS.GET_MESSAGE, (payload: NewMessageFromSocket) => {
-      socket.emit(SOCKET_EVENTS.RECEIVED_MESSAGE, {
-        groupId: payload.groupId,
-        messageId: payload.newMessage._id,
+    socket
+      .off(SOCKET_EVENTS.GET_MESSAGE)
+      .on(SOCKET_EVENTS.GET_MESSAGE, (payload: NewMessageFromSocket) => {
+        if (userId !== payload.newMessage.user._id) {
+          socket.emit(SOCKET_EVENTS.RECEIVED_MESSAGE, {
+            groupId: payload.groupId,
+            messageId: payload.newMessage._id,
+          });
+        }
+        handleAddNewMessage(payload);
       });
-      handleAddNewMessage(payload);
-    });
 
     socket.on(SOCKET_EVENTS.RECEIVED_MESSAGE, (payload) => {
       handleMessageReceived(payload);
@@ -47,6 +56,7 @@ export const useSocket = () => {
     handleUnTypingEvent,
     handleMessageReceived,
     handleMessageRead,
+    userId,
   ]);
 
   return socket;
