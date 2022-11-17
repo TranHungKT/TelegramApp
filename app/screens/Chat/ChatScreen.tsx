17003e2283daaf13addc08b276145f79e348c480
Hung Tran <tranhung26122612@gmail.com>
2022-11-17 17:01:55 +0700

    fixed ts

--- app/screens/Chat/ChatScreen.tsx
+++ app/screens/Chat/ChatScreen.tsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { LoadingComponent } from '@Components/index';
 import { AllGroupChatNavigationParamList } from '@Navigators/index';
 import { currentGroupSelector, groupsActions } from '@Stores/groups';
-import { getUserStatusByIdSelector, userIdSelector } from '@Stores/user';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
@@ -14,7 +13,7 @@ import { ListChatsContainer } from './containers/ListChatsContainer';
 
 export const ChatScreen = () => {
   const currentGroup = useSelector(currentGroupSelector);
-  const userId = useSelector(userIdSelector);
+
   const dispatch = useDispatch();
 
   const navigation =
@@ -23,11 +22,6 @@ export const ChatScreen = () => {
   const handleClickGoBack = () => {
     dispatch(groupsActions.setCurrentGroupId(''));
   };
-
-  const otherMember = currentGroup?.members.filter((member) => member._id !== userId);
-
-  const userStatusSelector = useSelector(getUserStatusByIdSelector);
-  const userStatus = userStatusSelector({ userId: otherMember ? otherMember[0]._id : '' });
 
   const handleClickName = () => {
     navigation.navigate('GroupChatInformationScreen');
@@ -39,14 +33,7 @@ export const ChatScreen = () => {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Header
-        name={currentGroup.name}
-        groupAvatar={currentGroup.groupAvatar}
-        totalMembers={currentGroup.members.length}
-        onClickGoBack={handleClickGoBack}
-        userStatus={userStatus}
-        onClickName={handleClickName}
-      />
+      <Header onClickGoBack={handleClickGoBack} onClickName={handleClickName} />
       <ListChatsContainer />
     </SafeAreaView>
   );
